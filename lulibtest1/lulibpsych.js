0f9734e371449eba8513a4bd31cfb82e51b09f7c
LulibPsyche <LulibPsyche@users.noreply.github.com>
2020-12-26 15:11:05 -0300

    changes

---
+++ lulibtest1/lulibpsych.js
@@ -0,0 +1,42 @@
+const comment = prompt("Pode escrever!");
+if (comment) {
+  console.log("Obrigada por participar!!!");
+} else {
+  console.log("Verifique o que escreveu, parece que não existe nenhum dado");
+}
+
+//While(input !== "quit" && input !== "q");
+//if (input === "list") {
+  //console.log("***********");
+  //for (let i = 0; i < todos.lenght; i++) {
+  //  console.log(`${i}: ${todos[i]}`);
+  //}
+
+  //console.log("************");
+//} else if (input === "new") {
+ // const newTodo = prompt("Ok, what is the new todo?");
+  //todos.push(newTodo);
+  //console.log(`${newTodo} added to the list!`);
+//} else if (input === "delete") {
+  //const index = prompt("Ok, enter an index to delete:");
+  //console.log(`Ok, deleted &{deleted[0]}`);
+//} else {
+  //console.log("unknow index")
+//}
+
+
+
+//function perolas(){
+  //console.log ('Vista a camisa!' 'sentimento de dono!')
+//}
+//perolas ();
+
+
+
+//function rant(message) {
+  //console.log(message.toUpperCase());
+  //console.log(message.toUpperCase());
+  //console.log(message.toUpperCase());
+//}
+
+
